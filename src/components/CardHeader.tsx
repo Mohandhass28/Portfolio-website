197cdd6329cb0ae551cb7f67ef10184e44247bcb
Mohandhass28 <mohandhass28@gmail.com>
2025-04-02 15:57:46 +0530

    remove unused var from the layout page

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,27 +1,24 @@
-import React from "react";
-import StartIcon from "@/assets/icons/star.svg";
-import { twMerge } from "tailwind-merge";
+import React from 'react'
+import StartIcon from '@/assets/icons/star.svg'
 
 const CardHeader = ({
   title,
   description,
-  className,
+  className
 }: {
-  title: string;
-  description: string;
-  className?: string;
+  title: string
+  description: string
+  className?: string
 }) => {
   return (
-    <div className={twMerge("flex flex-col p-6 md:py-8 md:px-10", className)}>
-      <div className="inline-flex items-center gap-2">
-        <StartIcon className="size-9 text-emerald-300" />
-        <h3 className="font-serif text-3xl ">{title}</h3>
+    <div className='flex flex-col'>
+      <div className='inline-flex items-center gap-2'>
+        <StartIcon className='size-9 text-emerald-300' />
+        <h3 className='font-serif text-3xl '>{title}</h3>
       </div>
-      <p className="text-sm lg:text-base lg:max-w-xs text-white/60 mt-2">
-        {description}
-      </p>
+      <p className='text-sm text-white/60 mt-2'>{description}</p>
     </div>
-  );
-};
+  )
+}
 
-export default CardHeader;
+export default CardHeader
